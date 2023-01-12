429140cd6a0c724b86bd37b89b19b4249c1993c0
solomkinav <92375862+solomkinav@users.noreply.github.com>
2023-01-12 10:20:22 +0300

    HW_Lesson3

---
+++ task3_2.py
@@ -0,0 +1,18 @@
+# 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры
+# как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.
+
+def user_info(name=None, surname=None, year=None, city=None, email=None, phone=None):
+    """ Выводит данные о пользователе """
+    print(f'Имя: {name}, Фамилия: {surname}, год рождения: {year}, '
+          f'город проживания: {city}, email: {email}, телефон: {phone}')
+
+
+# Вызов функции
+user_name = input("Введите имя пользователя: ")
+user_surname = input("Введите фамилию пользователя: ")
+user_year = int(input("Введите год рождения пользователя: "))
+user_city = input("Введите город проживания пользователя: ")
+user_email = input("Введите электронный адрес пользователя: ")
+user_phone = input("Введите телефон пользователя: ")
+user_info(name=user_name, year=user_year, surname=user_surname, city=user_city, email=user_email, phone=user_phone)
