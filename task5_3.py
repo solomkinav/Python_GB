c4ad0165d437a8bb81dc26b9181a97ec102d532e
solomkinav <92375862+solomkinav@users.noreply.github.com>
2023-01-23 10:29:38 +0300

    HW #5

---
+++ task5_3.py
@@ -0,0 +1,15 @@
+# 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину
+# их окладов. Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+# Выполнить подсчет средней величины дохода сотрудников.
+
+with open('lesson5_task3_text.txt', 'r', encoding='utf-8') as txt:
+    # создания словаря из данных в файле
+    salaries = {line.split()[0].strip(): int(line.split()[1]) for line in txt.readlines()}
+
+sum_of_salaries = 0  # начальное значение для суммы окладов
+for surname, salary in salaries.items():  # перебор словаря
+    sum_of_salaries += salary
+    if salary < 20000:
+        print(surname)
+average_salary = sum_of_salaries / len(salaries)
+print(f'Средняя величина дохода составляет {average_salary}')
